f398ea557e697dbc9bd36288516952154f97377e
Marius Augenstein <marius.augenstein@sap.com>
2017-09-20 16:04:55 +0200

    Rewritten to use promises. Version not bound to package version anymore. Therefore switched to 2.0.0

--- index.js
+++ index.js
@@ -1,83 +1,92 @@
 'use_strict';
 
-const http = require('http');
 const fs = require('fs');
 const fse = require('fs-extra');
 const url = require('url');
 const path = require('path');
-const mkdirp = require('mkdirp');
+const mkdirp = require('mkdirp-promise');
 const readdirp = require('readdirp');
 const request = require('request');
 const progress = require('request-progress');
 const AdmZip = require('adm-zip');
 const pretty = require('prettysize');
 const packageJson = require('./package.json');
+const isReachable = require('is-reachable');
 
-let oUrl = url.parse(`http://openui5.hana.ondemand.com/downloads/openui5-runtime-${packageJson.version}.zip`);
+let openUI5DownloadHost = packageJson.openui5.downloadHost;
+let oUrl = url.parse(`http://${openUI5DownloadHost}/downloads/openui5-runtime-${packageJson.openui5.version}.zip`);
 
 let downloadDir = path.resolve(__dirname + path.dirname(oUrl.pathname));
-let outDir = path.resolve(`${__dirname}/lib/${packageJson.version}`);
-if (!fs.existsSync(downloadDir)) {
-    mkdirp.sync(downloadDir);
-}
+let outDir = path.resolve(`${__dirname}/lib`);
+let outfile = path.resolve(__dirname + oUrl.pathname);
 
-if (!fs.existsSync(outDir)) {
-    mkdirp.sync(outDir);
-} else {
-    if (fs.existsSync(path.resolve(`${outDir}/resources/sap-ui-core.js`))) {
-        console.log('Already exists. Skipping download and extraction.');
-        return;
-    }
-}
-let oFile = path.resolve(__dirname + oUrl.pathname);
-
-new Promise((resolve, reject) => {
-    console.log(`Downloading ${oUrl.href} into ${outDir}`);
-    progress(request(oUrl.href))
-        .on('progress', state => {
-            let percent = Math.round(state.percent*100, 10);
-            console.log(`Downloaded: ${Math.round(state.percent*100, 10)}% [${pretty(state.size.transferred)} / ${pretty(state.size.total)}]`)
-        })
-        .on('error', err => {
-            reject(err);
-        })
-        .on('end', () => {
+Promise.all([fse.remove(outDir), fse.remove(downloadDir)])
+    .then(() => {
+        const p1 = mkdirp(downloadDir);
+        const p2 = mkdirp(outDir);
+        const p3 = isReachable(openUI5DownloadHost);
+        return Promise.all([p1, p2, p3]);
+    })
+    .then(values => {
+        const reachable = values[2];
+        if (!reachable) {
+            throw new Error('OpenUI5 download host not reachable. Try setting proxy (i.e. export HTTP_PROXY=http://proxy:8080)');
+        }
+        return new Promise((resolve, reject) => {
+            console.log(`Downloading ${oUrl.href} into ${outDir}`);
+            progress(request(oUrl.href))
+                .on('progress', state => {
+                    let percent = Math.round(state.percent * 100, 10);
+                    console.log(
+                        `Downloaded: ${Math.round(state.percent * 100, 10)}% [${pretty(
+                            state.size.transferred
+                        )} / ${pretty(state.size.total)}]`
+                    );
+                })
+                .on('error', err => {
+                    reject(err);
+                })
+                .on('end', () => {
+                    resolve();
+                })
+                .pipe(fs.createWriteStream(outfile));
+        });
+    })
+    .then(() => {
+        console.log(`Starting extraction of '${outfile}' into '${outDir}'...`);
+        return new Promise((resolve, reject) => {
+            var zip = new AdmZip(outfile);
+            zip.extractAllTo(outDir, true);
             resolve();
-        })
-        .pipe(fs.createWriteStream(oFile));
-}).then(() => {
-    console.log(`Starting extraction of '${oFile}' into '${outDir}'...`);
-    return new Promise((resolve, reject) => {
-        var zip = new AdmZip(oFile);
-        zip.extractAllTo(outDir, true);
-        resolve();
+        });
+    })
+    .then(() => {
+        console.log('Cleanup: removing dbg files');
+        let i = 0;
+        let delSize = 0;
+        let aRemoves = [];
+        return new Promise((resolve, reject) => {
+            readdirp({root: outDir, fileFilter: '*-dbg.js'})
+                .on('data', function(entry) {
+                    aRemoves.push(fse.remove(entry.fullPath));
+                    i++;
+                    delSize += entry.stat.size;
+                })
+                .on('error', err => {
+                    reject(err);
+                })
+                .on('end', () => {
+                    Promise.all(aRemoves).then(() => {
+                        console.log(`Cleanup: ${i} files removed. Saved ${pretty(delSize)}.`);
+                        resolve();
+                    });
+                });
+        });
+    })
+    .then(() => {
+        console.log('Cleanup: removing downloads');
+        return fse.remove(downloadDir);
+    })
+    .catch(function(err) {
+        console.log(err);
     });
-}).then(() => {
-    console.log('Cleanup: removing dbg files');
-    let i = 0;
-    let delSize = 0;
-    return new Promise((resolve, reject) => {
-        readdirp({ root: outDir, fileFilter: '*-dbg.js' })
-            .on('data', function (entry) {
-                console.log(`Removing ${entry.fullPath}`);
-                fse.removeSync(entry.fullPath);
-                i++;
-                delSize += entry.stat.size;
-            })
-            .on('error', (err) => {
-                reject(err);
-            })
-            .on('end', () => {
-                console.log(`Cleanup: ${i} files removed. Saved ${pretty(delSize)}.`);
-                resolve();
-            })
-    });
-}).then(() => {
-    console.log('Cleanup: removing downloads');
-    return new Promise((resolve, reject) => {
-        fse.removeSync(downloadDir);
-        resolve();
-    });
-}).catch(function(err) {
-    console.log(err);
-});
