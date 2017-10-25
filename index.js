90dd96754385c08dde3ad170c298724a33e19ef3
Marius Augenstein <marius.augenstein@sap.com>
2017-10-25 12:36:30 +0200

    Refactoring to use latest version keyword of released openui5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use_strict';
+'use strict';
 
 const fs = require('fs');
 const fse = require('fs-extra');
@@ -7,22 +7,46 @@ const path = require('path');
 const mkdirp = require('mkdirp-promise');
 const readdirp = require('readdirp');
 const request = require('request');
+const rpn = require('request-promise-native');
 const progress = require('request-progress');
 const AdmZip = require('adm-zip');
 const pretty = require('prettysize');
 const packageJson = require('./package.json');
 
-let openUI5DownloadHost = packageJson.openui5.downloadHost;
-let oUrl = url.parse(`http://${openUI5DownloadHost}/downloads/openui5-runtime-${packageJson.openui5.version}.zip`);
+const openUI5DownloadHost = packageJson.openui5.downloadHost;
+const oUI5VersionUrl = url.parse(`http://openui5.org/OpenUI5Downloads.json`);
+const oUrl = url.parse(`http://${openUI5DownloadHost}/downloads/openui5-runtime-${packageJson.openui5.version}.zip`);
 
-let downloadDir = path.resolve(__dirname + path.dirname(oUrl.pathname));
-let outDir = path.resolve(`${__dirname}/lib`);
-let outfile = path.resolve(__dirname + oUrl.pathname);
+const downloadDir = path.resolve(`${__dirname}${path.dirname(oUrl.pathname)}`);
+const outDir = path.resolve(`${__dirname}/lib`);
+const outfile = path.resolve(`${__dirname}${oUrl.pathname}`);
 
-Promise.all([fse.remove(outDir), fse.remove(downloadDir)])
-    .then(() => {
+function comp(a, b) {
+    if (a.version < b.version) return -1;
+    if (a.version > b.version) return 1;
+    return 0;
+}
+
+function calcLatest(aData) {
+    const bStable = packageJson.openui5.stable && packageJson.openui5.stable === 'true';
+
+    const aRet = aData.filter(elem => {
+        if (!bStable) {
+            return elem.beta && elem.beta === 'true';
+        } else {
+            return !elem.beta;
+        }
+    });
+    return aRet.sort(comp)[aRet.length - 1].version;
+}
+
+Promise.all([rpn.get(oUI5VersionUrl.href), fse.remove(outDir), fse.remove(downloadDir)])
+    .then(data => {
+        packageJson.openui5.version =
+            packageJson.openui5.version === 'latest' ? calcLatest(JSON.parse(data[0])) : packageJson.openui5.version;
         const p1 = mkdirp(downloadDir);
         const p2 = mkdirp(outDir);
+        process.exit(0);
         return Promise.all([p1, p2]);
     })
     .then(values => {
@@ -30,7 +54,6 @@ Promise.all([fse.remove(outDir), fse.remove(downloadDir)])
             console.log(`Downloading ${oUrl.href} into ${outDir}`);
             progress(request(oUrl.href))
                 .on('progress', state => {
-                    let percent = Math.round(state.percent * 100, 10);
                     console.log(
                         `Downloaded: ${Math.round(state.percent * 100, 10)}% [${pretty(
                             state.size.transferred
@@ -58,7 +81,7 @@ Promise.all([fse.remove(outDir), fse.remove(downloadDir)])
         console.log('Cleanup: removing dbg files');
         let i = 0;
         let delSize = 0;
-        let aRemoves = [];
+        const aRemoves = [];
         return new Promise((resolve, reject) => {
             readdirp({root: outDir, fileFilter: '*-dbg.js'})
                 .on('data', function(entry) {
